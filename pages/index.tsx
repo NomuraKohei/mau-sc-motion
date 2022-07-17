4373f14a6de3fce500691ee509fe76b00996d1a6
NomuraKohei <seiryu2nd@gmail.com>
2022-07-17 15:22:52 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import data2 from '../public/data2.json';
 import data3 from '../public/data3.json';
 import data4 from '../public/data4.json';
 import data5 from '../public/data5.json';
+import data6 from '../public/data6.json';
+import data7 from '../public/data7.json';
 import MotionSection from './components/MotionSection';
 
 
@@ -29,6 +31,8 @@ const Home: NextPage = () => {
           <MotionSection animationData={data3} title="ボヨーン" width={55} height={55} top={20} left={155} isHalf />
           <MotionSection animationData={data4} title="スポン" width={80} height={80} top={95} left={160} isHalf />
           <MotionSection animationData={data5} title="パリン" width={120} height={115} top={69} left={140} isHalf isRect />
+          <MotionSection animationData={data6} title="ガッチン" width={120} height={115} top={20} left={133} isHalf isRect />
+          <MotionSection animationData={data7} title="ペチャッ" width={70} height={70} top={83} left={155} isHalf />
         </div>
       </main>
     </div>
